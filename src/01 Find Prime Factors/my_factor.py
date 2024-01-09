90ef99732323681946160abd7ab970ca61bcf8ad
Anel Danza <anelguerrero23@gmail.com>
2024-01-09 22:01:05 +0000

    write find_prime_factors function

--- src/01 Find Prime Factors/my_factor.py
+++ src/01 Find Prime Factors/my_factor.py
@@ -12,23 +12,45 @@ def is_prime(num):
   
   return True
 
+def get_next_prime(num):
+  found_prime = 0
+  i = 1
+
+  while not found_prime:
+    if is_prime(num + i):
+      found_prime = num + i
+      break
+    i += 1
+  
+  return found_prime
+
+
 def find_prime_factors(num):
-  print(f'you passed type {type(num)}')
+  output = list()
 
   if type(num) != int or num < 1:
     print("pass only positive itegers to the function")
-    return
-  
+    return output
+
+  next_prime = 2
+
   if is_prime(num):
-    return num
+    output.append(num)
+  else:
+    while num % next_prime > 0:
+      next_prime = get_next_prime(next_prime)
+    
+    output.append(next_prime)
+    remainder = int(num / next_prime)
+    right = find_prime_factors(remainder)
+    output.extend(right)
+    
+  return output
   
 
   
 
-# find_prime_factors("string")
-print(is_prime(17))
-print(is_prime(18))
-print(is_prime(19))
-print(is_prime(20))
-print(is_prime(45))
+print(find_prime_factors(630))
+
+
 
